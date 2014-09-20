de54061c8a880097a723a395d4ed0277dbc2a6b2
ninjamu <nguoiyeumoicuatoi_2005@yahoo.com>
2014-09-20 10:36:07 +0700

    add slideshow,opacity when on mouse over images

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+	$('.img img:gt(0)').hide();
+	setInterval(function(){
+		$('.img :first-child').fadeOut()
+		.next('img').fadeIn()
+		.end().appendTo('.img');
+	},4000);
+	$('.cola').hover(function(){
+		$('.text').animate({
+			opacity:'toggle'
+		})
+	})
 })
